f95210c6691122cc1f3f16a4a552af5d86f4d903
AhmedOsama9 <ahmedosamaa975@gmail.com>
2024-01-30 07:02:53 +0200

    updated the name

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,18 @@
-const AddUserObjectTest = (req, res) => {
+const User = require("../db");
+
+const test = (req, res) => {
     res.json({message: "The test works fine"});
 }
 
+const AddUserObjectTest = (req, res) => {
+    const userObject = {
+        name: "John",
+        phone: "123456789",
+        email: "ahmed@gmail.com"
+    }
+}
 
 module.exports = {
+    test,
     AddUserObjectTest,
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const {
+    test,
     AddUserObjectTest, 
 } = require("../controllers/userController");
 
 
-router.get("/test", AddUserObjectTest);
+router.get("/test", test);
 
 module.exports = router;
